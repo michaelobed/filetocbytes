04f459723db20aa2d5aadf348c32e6764ed0b4d0
Michael Obed <mikee246@live.co.uk>
2025-06-10 23:47:51 +0100

    Added support for custom output name, array name and textual mode.

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,32 @@
 
 int main(int argc, const char* argv[])
 {
+    std::string arrayName = "bytes";
     char buf[6];
     int byteCtr = 0;
     std::vector<uint8_t> bytes;
+    bool isText = false;
+    std::string outputPath = "./out.c";
 
     /* Bail out if input file is not given. */
     if(argc < 2)
         return -1;
+
+    /* Parse args. */
+    for(int i = 1; i < argc; i++)
+    {
+        /* Textual mode (will append '\0' to bytestream). */
+        if(std::string(argv[i]) == "-t")
+            isText = true;
+
+        /* Array name (optional, defaults to bytes[]). */
+        else if(std::string(argv[i]) == "-n")
+            arrayName = argv[i + 1];
+
+        /* Output path (optional, defaults to "./out.c"). */
+        else if(std::string(argv[i]) == "-o")
+            outputPath = argv[i + 1];
+    }
 
     /* Open input file. */
     std::fstream f(argv[1], std::ios::in | std::ios::binary);
@@ -26,13 +45,15 @@ int main(int argc, const char* argv[])
     while(!f.eof())
         bytes.push_back(f.get());
     bytes.pop_back();
+    if(isText)
+        bytes.push_back(0x00);
     f.close();
 
     /* Create an output file. */
-    f.open("./out.txt", std::ios::out);
+    f.open(outputPath, std::ios::out);
 
     /* Usual C-style fluff. */
-    f << "uint8_t bytes[" << std::to_string(bytes.size()) << "] = \n{\n\t";
+    f << "uint8_t " << arrayName << "[" << std::to_string(bytes.size()) << "] = \n{\n\t";
 
     /* Group bytes into rows of 8 for neatness. */
     for(uint8_t b : bytes)
